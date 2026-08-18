38c07935baf079aae0569f39d717bf0bb70a00fa
djcaptaind <djcaptaind@gmail.com>
2026-08-18 17:47:12 -0500

    Update Firebase configuration with project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
 // Paste your Firebase Web App configuration here.
 // Firebase Console > Project Settings > Your apps > Web app > SDK setup and configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT",
-  storageBucket: "PASTE_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyAEnpJF_3R14l25K9Zd3MltfbdAI1erQfM",
+  authDomain: "callaway-jrotc-answer-game.firebaseapp.com",
+  databaseURL: "https://callaway-jrotc-answer-game-default-rtdb.firebaseio.com",
+projectId: "callaway-jrotc-answer-game",
+  storageBucket: "callaway-jrotc-answer-game.firebasestorage.app",
+  messagingSenderId: "950581678170",
+  appId: "1:950581678170:web:de5badece27b8723a58863",
+  measurementId: "G-50V8JLFD33"
 };
