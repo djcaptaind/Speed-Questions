9de5a8a051b37de470ebc56c23a8ed474388f98d
djcaptaind <djcaptaind@gmail.com>
2026-08-19 19:21:10 -0500

    Update Firebase configuration for the project

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// Paste your Firebase Web App configuration here.
-// Firebase Console > Project Settings > Your apps > Web app > SDK setup and configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Callaway JROTC Answer Game - Firebase Configuration
+
 window.FIREBASE_CONFIG = {
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
   apiKey: "AIzaSyAEnpJF_3R14l25K9Zd3MltfbdAI1erQfM",
   authDomain: "callaway-jrotc-answer-game.firebaseapp.com",
+  databaseURL: "https://callaway-jrotc-answer-game-default-rtdb.firebaseio.com/",
   projectId: "callaway-jrotc-answer-game",
   storageBucket: "callaway-jrotc-answer-game.firebasestorage.app",
   messagingSenderId: "950581678170",
