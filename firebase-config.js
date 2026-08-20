dcd366bf077fc98046565952dd354935e15ca875
djcaptaind <djcaptaind@gmail.com>
2026-08-19 20:02:21 -0500

    Update Firebase config to use window.FIREBASE_CONFIG

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyAEnpJF_3R14l25K9Zd3MltfbdAI1erQfM",
   authDomain: "callaway-jrotc-answer-game.firebaseapp.com",
+  databaseURL: "https://callaway-jrotc-answer-game-default-rtdb.firebaseio.com/",
   projectId: "callaway-jrotc-answer-game",
   storageBucket: "callaway-jrotc-answer-game.firebasestorage.app",
   messagingSenderId: "950581678170",
