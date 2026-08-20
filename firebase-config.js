c067321ab9359faaf5b092840c754025fc3faab5
djcaptaind <djcaptaind@gmail.com>
2026-08-19 19:09:38 -0500

    Refactor Firebase config to include measurementId
    Updated Firebase configuration structure to include measurementId as optional.

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,11 @@
 // Firebase Console > Project Settings > Your apps > Web app > SDK setup and configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 window.FIREBASE_CONFIG = {
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAEnpJF_3R14l25K9Zd3MltfbdAI1erQfM",
   authDomain: "callaway-jrotc-answer-game.firebaseapp.com",
-  databaseURL: "https://callaway-jrotc-answer-game-default-rtdb.firebaseio.com",
-projectId: "callaway-jrotc-answer-game",
+  projectId: "callaway-jrotc-answer-game",
   storageBucket: "callaway-jrotc-answer-game.firebasestorage.app",
   messagingSenderId: "950581678170",
   appId: "1:950581678170:web:de5badece27b8723a58863",
